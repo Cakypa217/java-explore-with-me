69a1d9f4b8c63533d4ca82508c6c009d9e94f004
Igor217 <rkjdth.ctv@yandex.ru>
2025-02-09 13:16:24 +0300

    Checkstyle 1.2

--- stats/stats-server/src/main/java/ru/practicum/StatsServerApplication.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServerApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatsServerApplication  {
     public static void main(String[] args) {
-        SpringApplication.run(StatsServerApplication .class, args);
+        SpringApplication.run(StatsServerApplication.class, args);
     }
 }
